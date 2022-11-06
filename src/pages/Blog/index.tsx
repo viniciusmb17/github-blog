fd7b7d9fcc95e5f922518a1784e20a732ba06e50
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-11-06 16:35:53 -0300

    feat: start Profile

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,5 +1,35 @@
-import { BlogContainer } from './style'
+import {
+  BlogContainer,
+  ProfileAvatar,
+  ProfileContent,
+  ProfileHeader,
+  ProfileInfo,
+} from './style'
 
 export function Blog() {
-  return <BlogContainer>GitHub Blog</BlogContainer>
+  return (
+    <BlogContainer>
+      <ProfileInfo>
+        <ProfileAvatar src="https://github.com/viniciusmb17.png" alt="" />
+        <ProfileContent>
+          <ProfileHeader>
+            <h1>Vinicius Barbosa</h1>
+            <a href="#">GITHUB</a>
+          </ProfileHeader>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus,
+            deserunt veritatis, nihil enim impedit distinctio doloremque
+            reiciendis nam voluptas odio harum eius repellat ipsam esse quia
+            dignissimos officiis illum suscipit!
+          </p>
+          <ul>
+            <li>viniciusmb17</li>
+            <li>Empresa</li>
+            <li>5 seguidores</li>
+          </ul>
+        </ProfileContent>
+      </ProfileInfo>
+      GitHub Blog
+    </BlogContainer>
+  )
 }
--- src/pages/Blog/style.ts
+++ src/pages/Blog/style.ts
@@ -2,5 +2,46 @@ import styled from 'styled-components'
 
 export const BlogContainer = styled.div`
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
   width: 90rem;
+  align-items: center;
+`
+
+export const ProfileInfo = styled.section`
+  width: 54rem;
+  height: 13.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme['base-profile']};
+  box-shadow: 0 2px 28px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  margin-top: -5.5rem;
+  padding: 40px 32px;
+  gap: 32px;
+`
+export const ProfileAvatar = styled.img`
+  width: 9.25rem;
+  height: 9.25rem;
+  border-radius: 8px;
+`
+export const ProfileHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  h1 {
+    font-weight: 700;
+    font-size: 1.5rem;
+    line-height: 130%;
+
+    color: ${(props) => props.theme['base-title']};
+  }
+`
+
+export const ProfileContent = styled.div`
+  ul {
+    display: flex;
+    list-style-type: none;
+    gap: 1rem;
+  }
 `
