c1223d21f051ff9ee6486a0acdefdf5ca5250700
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-12-06 00:55:13 -0300

    feat: react-markdown and SyntaxHighlighter

--- src/pages/PostPage/components/PostContent/index.tsx
+++ src/pages/PostPage/components/PostContent/index.tsx
@@ -1,74 +1,34 @@
-import {
-  CodeComment,
-  CodeContainer,
-  CodeType,
-  CodeValue,
-  PostContentContainer,
-  PostSection,
-} from './style'
+import { PostContentContainer, customDracula } from './style'
+import ReactMarkdown from 'react-markdown'
+import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/prism'
+import { CodeProps } from 'react-markdown/lib/ast-to-react'
 
-const ONE_SPACE = <span>&nbsp;</span>
-const TWO_SPACES = <span>&ensp;</span>
-const FOUR_SPACES = <span>&emsp;</span>
-
-export function PostContent() {
+export function PostContent({ content }: { content: string }) {
   return (
     <PostContentContainer>
-      <PostSection>
-        <p>
-          <strong>
-            Programming languages all have built-in data structures, but these
-            often differ from one language to another.
-          </strong>
-          {ONE_SPACE}
-          This article attempts to list the built-in data structures available
-          in JavaScript and what properties they have. These can be used to
-          build other data structures. Wherever possible, comparisons with other
-          languages are drawn.
-        </p>
-      </PostSection>
-      <PostSection>
-        <p>
-          <a href="#"> Dynamic typing </a>
-        </p>
-        <p>
-          JavaScript is a loosely typed and dynamic language. Variables in
-          JavaScript are not directly associated with any particular value type,
-          and any variable can be assigned (and re-assigned) values of all
-          types:
-        </p>
-      </PostSection>
-      <PostSection>
-        <CodeContainer>
-          <p>
-            <CodeType>let</CodeType> foo = <CodeValue>42</CodeValue>;
-            <CodeComment>
-              {FOUR_SPACES}
-              {FOUR_SPACES}
-              {` // foo is now a number`}
-            </CodeComment>
-          </p>
-          <p>
-            foo = <CodeValue>{`'bar'`}</CodeValue>;
-            <CodeComment>
-              {FOUR_SPACES}
-              {FOUR_SPACES}
-              {FOUR_SPACES}
-              {`// foo is now a string`}
-            </CodeComment>
-          </p>
-          <p>
-            foo = <CodeValue>{`true`}</CodeValue>;
-            <CodeComment>
-              {FOUR_SPACES}
-              {FOUR_SPACES}
-              {FOUR_SPACES}
-              {TWO_SPACES}
-              {`// foo is now a boolean`}
-            </CodeComment>
-          </p>
-        </CodeContainer>
-      </PostSection>
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }: CodeProps) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={customDracula}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </PostContentContainer>
   )
 }
--- src/pages/PostPage/components/PostContent/style.ts
+++ src/pages/PostPage/components/PostContent/style.ts
@@ -25,13 +25,159 @@ export const CodeContainer = styled.div`
   line-height: 160%;
   background-color: ${(props) => props.theme['base-post']};
 `
-
-export const CodeType = styled.span`
-  color: #80abd6;
-`
-export const CodeValue = styled.span`
-  color: #6ad445;
-`
-export const CodeComment = styled.span`
-  color: #4f6173;
-`
+export const customDracula = {
+  'code[class*="language-"]': {
+    color: '#AFC2D4',
+    background: 'none',
+    textShadow: '0 1px rgba(0, 0, 0, 0.3)',
+    fontFamily:
+      "'Fira Code', Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+    textAlign: 'left',
+    whiteSpace: 'pre',
+    wordSpacing: 'normal',
+    wordBreak: 'normal',
+    wordWrap: 'normal',
+    lineHeight: '1.6',
+    MozTabSize: '4',
+    OTabSize: '4',
+    tabSize: '4',
+    WebkitHyphens: 'none',
+    MozHyphens: 'none',
+    msHyphens: 'none',
+    hyphens: 'none',
+  },
+  'pre[class*="language-"]': {
+    color: '#AFC2D4',
+    background: '#112131',
+    textShadow: '0 1px rgba(0, 0, 0, 0.3)',
+    fontFamily:
+      "'Fira Code', Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+    textAlign: 'left',
+    whiteSpace: 'pre',
+    wordSpacing: 'normal',
+    wordBreak: 'normal',
+    wordWrap: 'normal',
+    lineHeight: '1.6',
+    MozTabSize: '4',
+    OTabSize: '4',
+    tabSize: '4',
+    WebkitHyphens: 'none',
+    MozHyphens: 'none',
+    msHyphens: 'none',
+    hyphens: 'none',
+    padding: '1rem',
+    margin: '0',
+    overflow: 'auto',
+    borderRadius: '2px',
+  },
+  ':not(pre) > code[class*="language-"]': {
+    background: '#282a36',
+    padding: '.1em',
+    borderRadius: '2px',
+    whiteSpace: 'normal',
+  },
+  comment: {
+    color: '#4f6173',
+  },
+  prolog: {
+    color: '#6272a4',
+  },
+  doctype: {
+    color: '#6272a4',
+  },
+  cdata: {
+    color: '#6272a4',
+  },
+  punctuation: {
+    color: '#AFC2D4',
+  },
+  '.namespace': {
+    Opacity: '.7',
+  },
+  property: {
+    color: '#ff79c6',
+  },
+  tag: {
+    color: '#ff79c6',
+  },
+  constant: {
+    color: '#ff79c6',
+  },
+  symbol: {
+    color: '#ff79c6',
+  },
+  deleted: {
+    color: '#ff79c6',
+  },
+  boolean: {
+    color: '#6ad445',
+  },
+  number: {
+    color: '#6ad445',
+  },
+  selector: {
+    color: '#6ad445',
+  },
+  'attr-name': {
+    color: '#6ad445',
+  },
+  string: {
+    color: '#6ad445',
+  },
+  char: {
+    color: '#6ad445',
+  },
+  builtin: {
+    color: '#6ad445',
+  },
+  inserted: {
+    color: '#6ad445',
+  },
+  operator: {
+    color: '#AFC2D4',
+  },
+  entity: {
+    color: '#AFC2D4',
+    cursor: 'help',
+  },
+  url: {
+    color: '#AFC2D4',
+  },
+  '.language-css .token.string': {
+    color: '#AFC2D4',
+  },
+  '.style .token.string': {
+    color: '#AFC2D4',
+  },
+  variable: {
+    color: '#AFC2D4',
+  },
+  atrule: {
+    color: '#f1fa8c',
+  },
+  'attr-value': {
+    color: '#f1fa8c',
+  },
+  function: {
+    color: '#f1fa8c',
+  },
+  'class-name': {
+    color: '#f1fa8c',
+  },
+  keyword: {
+    color: '#80ABD6',
+  },
+  regex: {
+    color: '#ffb86c',
+  },
+  important: {
+    color: '#ffb86c',
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  italic: {
+    fontStyle: 'italic',
+  },
+}
