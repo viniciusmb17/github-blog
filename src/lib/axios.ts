4b61ae05709042d88ad3a1f0b1a8090134031d00
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-02-18 18:12:40 -0300

    feat: github token api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,3 +3,13 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'https://api.github.com/',
 })
+
+api.interceptors.request.use(async (config) => {
+  const token = import.meta.env.VITE_GITHUB_TOKEN
+
+  if (token) {
+    api.defaults.headers.common.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
